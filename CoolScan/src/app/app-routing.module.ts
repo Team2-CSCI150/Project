db4ec201089f2a0fa21041d1f290c079262f45c7
Jese Hernandez <jesc32@mail.fresnostate.edu>
2019-10-11 14:15:40 -0700

    Barebones file creation

--- CoolScan/src/app/app-routing.module.ts
+++ CoolScan/src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'test', loadChildren: './test/test.module#TestPageModule' },
-  { path: 'test', loadChildren: './test/test.module#TestPageModule' },
-  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
-  { path: 'planner', loadChildren: './planner/planner.module#PlannerPageModule' },
   { path: 'inbox', loadChildren: './inbox/inbox.module#InboxPageModule' },
+  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'planner', loadChildren: './planner/planner.module#PlannerPageModule' },
   { path: 'grades', loadChildren: './grades/grades.module#GradesPageModule' },
   { path: 'check-in', loadChildren: './check-in/check-in.module#CheckInPageModule' },
 ];
