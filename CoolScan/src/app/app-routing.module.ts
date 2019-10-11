f016cb52624e7c64edf0329c8bd95e4890f1cc49
Jese Hernandez <jesc32@mail.fresnostate.edu>
2019-10-11 13:59:13 -0700

    Barebones created

--- CoolScan/src/app/app-routing.module.ts
+++ CoolScan/src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'test', loadChildren: './test/test.module#TestPageModule' },
+  { path: 'test', loadChildren: './test/test.module#TestPageModule' },
+  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
+  { path: 'planner', loadChildren: './planner/planner.module#PlannerPageModule' },
+  { path: 'inbox', loadChildren: './inbox/inbox.module#InboxPageModule' },
+  { path: 'grades', loadChildren: './grades/grades.module#GradesPageModule' },
+  { path: 'check-in', loadChildren: './check-in/check-in.module#CheckInPageModule' },
 ];
 
 @NgModule({
