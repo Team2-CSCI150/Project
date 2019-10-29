b6c3461373c1818c2090854ae1551144dc19b817
Jese Hernandez <jesc32@mail.fresnostate.edu>
2019-10-29 15:18:29 -0700

    GPS module initial implementation

--- CoolScan/src/app/check-in/check-in.page.ts
+++ CoolScan/src/app/check-in/check-in.page.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-check-in',
   templateUrl: './check-in.page.html',
   styleUrls: ['./check-in.page.scss'],
 })
+
+
+    var onSuccess = function(position) {
+            alert('Latitude: '          + position.coords.latitude          + '\n' +
+	                  'Longitude: '         + position.coords.longitude         + '\n' +
+			                'Altitude: '          + position.coords.altitude          + '\n' +
+					              'Accuracy: '          + position.coords.accuracy          + '\n' +
+						                    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+								                  'Heading: '           + position.coords.heading           + '\n' +
+										                'Speed: '             + position.coords.speed             + '\n' +
+												              'Timestamp: '         + position.timestamp                + '\n');
+													          };
+
+														      // onError Callback receives a PositionError object
+														          //
+															      function onError(error) {
+															              alert('code: '    + error.code    + '\n' +
+																                    'message: ' + error.message + '\n');
+																		        }
+
+																			    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+
 export class CheckInPage implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
 
   //
   checkIn() {
-  	document.getElementById('demo').innerHTML = "Hello";
+    document.getElementById('demo').innerHTML = "Button Clicked!";
   }
 
 }
