1d496c6235357eb47a1191ec98bf1930c1e0f799
Jese Hernandez <jesc32@mail.fresnostate.edu>
2019-10-29 15:29:33 -0700

    Cleaned up code

--- CoolScan/src/app/check-in/check-in.page.ts
+++ CoolScan/src/app/check-in/check-in.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-
 @Component({
   selector: 'app-check-in',
   templateUrl: './check-in.page.html',
