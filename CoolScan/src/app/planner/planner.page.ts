827efe50950a6ec4c26cfce2763204463e909eea
taniaks <35944188+taniaks@users.noreply.github.com>
2019-10-31 19:44:12 -0700

    updated script function

--- CoolScan/src/app/planner/planner.page.ts
+++ CoolScan/src/app/planner/planner.page.ts
@@ -1,21 +1,57 @@
-import { Component} from '@angular/core';
+
 import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { NavController, AlertController, ModalController } from '@ionic/angular';
+//import * as moment from 'moment';
 @Component({
   selector: 'app-planner',
   templateUrl: './planner.page.html',
   styleUrls: ['./planner.page.scss'],
 })
-export class PlannerPage {
+export class PlannerPage implements OnInit{
+  event = {
+	id:'',
+	title:'',
+	desc: '',
+	start:'',
+	end: '',
+	allDay: false
+  };
+  
+  minDate= new Date().toISOString();
+  
   eventSource = [];
   calendar = {
     mode: 'month',
     currentDate: new Date()
   };
+  viewTitle= '';
+  
+  @ViewChild(CalendarComponent, {static: false}) myCal: CalendarComponent;
+  constructor() {
+  }
+  
+  ngOnInit(){
+	this.resetEvent();  
+  }
+  resetEvent(){
+	this.event = {
+		id:'',
+		title:'',
+		desc: '',
+		start: new Date().toISOString(),
+		end: new Date().toISOString(),
+		allDay: false
+	};		
+  }
+  
+  addEvent(){
+	  
+  }
   
   onViewTitleChanged() {
-    
+  
   }
   onEventSelected() {
     
