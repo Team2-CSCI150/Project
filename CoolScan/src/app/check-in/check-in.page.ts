e18fd3b8c09b668796574ebbd5bb7a7422aa8d51
Jese Hernandez <jesc32@mail.fresnostate.edu>
2019-11-07 16:05:12 -0800

    Updated CheckIn() to ensure device is in target location

--- CoolScan/src/app/check-in/check-in.page.ts
+++ CoolScan/src/app/check-in/check-in.page.ts
@@ -13,11 +13,27 @@ export class CheckInPage implements OnInit {
   }
 
   checkIn() {
-    //document.getElementById('demo').innerHTML = "Button clicked!";
+    //GPS initialization
     this.geolocation.getCurrentPosition().then((resp) => {
+      
+      //Definition of classroom coordinates
+      //36.814, -119.75
+      let tgtLatitude = 36.814;
+      let tgtLongitude = -119.75;
+
+      //Retrieval of student's coordinates
       document.getElementById('demo1').innerHTML = "Latitude: " + resp.coords.latitude.toString();
       document.getElementById('demo2').innerHTML = "Longitude: " + resp.coords.longitude.toString();
       console.log(resp.coords.latitude, resp.coords.longitude);
+
+      //Check if coordinates are the same
+      //toPrecision() rounds the number to specified length (5 in this case)
+      //rounding is needed because different devices provide varying degrees of precision 
+      if(resp.coords.latitude.toPrecision(5) == tgtLatitude) {
+        if(resp.coords.longitude.toPrecision(5) == tgtLongitude) {
+          document.getElementById('demo3').innerHTML = "Check-in Success!";
+	}
+      }
     }).catch((error) => {
       console.log('Error getting location', error);
     });
