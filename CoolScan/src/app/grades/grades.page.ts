49cf38e2d35f0e1051efe94b8ce25ec8bfb9fa16
kevinwp55 <47406156+kevinwp55@users.noreply.github.com>
2019-11-12 17:16:41 -0800

    home/grades/grade-report
    Adjusted login and register to retain user data throughout app navigation.
    Adjusted grades and grade-report templates to fit the data structure from php files.

--- CoolScan/src/app/grades/grades.page.ts
+++ CoolScan/src/app/grades/grades.page.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import { MenuController } from '@ionic/angular';
+
 @Component({
   selector: 'app-grades',
   templateUrl: './grades.page.html',
@@ -11,32 +13,89 @@ import { map } from 'rxjs/operators';
 })
 export class GradesPage implements OnInit {
 
-	classes = [
-		{
-		title: 'Class 1',
-		score: '100'
-		},
-		{
-		title: 'Class 2',
-		score: '80'
-		}
-	]
 	constructor(public alertCtrl: AlertController, 
   				public navCtrl: NavController,
   				private router: Router, 
   				public load: LoadingController,
   				private http: HttpClient	
-  				) { }
-  				
-	ngOnInit() { }
+  				) { 
+  				}
 
-  
-	StudentID = 0;
-	ClassName = " ";
-	
-	ViewCurrentGrade(StudentID, ClassName){
-		//return grades for StudnetID from ClassID
-	}
+	getEntriesUrl = 'http://localhost/csci150/getEntries.php';
 
+	classIDs = [];
+	classKeys = [];
+	displayClasses = [];
+	studentID;
 
+	ngOnInit() {
+		//console.log("In Grades TS");
+		this.studentID = sessionStorage.getItem("UserID");
+		//console.log(this.studentID);
+		this.classIDs = JSON.parse(sessionStorage.getItem('classes'));
+		//console.log(this.classIDs);
+		this.classKeys = Object.keys(this.classIDs);
+		//console.log(this.classKeys);
+		
+		for(var i=0; i<this.classKeys.length; i++)
+		{
+			this.getEntries(this.studentID,this.classKeys[i],this.classIDs[this.classKeys[i]]);
+		}
+    }
+
+    async presentGetEntriesError(error) {
+    const alert = await this.alertCtrl.create({
+      header: 'Failed To Get Entries',
+      message: 'Cannot Get Error: ' + error,
+      buttons: ['OK'],
+    });
+    
+    await alert.present();
+    let result=await alert.onDidDismiss();
+    console.log(result);
+    }
+
+    getEntries(student,classIndx,classname)
+    {
+    	let res;
+    	let data = JSON.stringify({
+    		'studentID': student,
+    		'classID': classIndx,
+    		'showWhat': 'grades'
+    	});
+    	//console.log("Data: " + data);
+    	this.http.post(this.getEntriesUrl, data).subscribe(res=>{
+        	if(res[0] == 'Get Grade Success!')
+        	{
+          		let temp = {
+          			'title': classname,
+          			'score': Number.parseFloat(res[1]).toPrecision(3)
+          		};
+          		this.displayClasses.push(temp);
+        	}
+        	else
+        	{
+          	console.log(this.presentGetEntriesError(res[0]));
+      		}
+      	}, error => {
+          	console.log(this.presentGetEntriesError(error));
+        });
+    }
+
+    goTo(classname)
+    {
+    	var key;
+    	for(var i=0; i<this.classKeys.length; i++)
+    	{
+    		if(this.classIDs[this.classKeys[i]] == classname)
+    		{
+    			key = this.classKeys[i];
+    		}
+    	}
+    	
+    	let classEntry = {'name': classname, 'classid': key};
+    	//console.log(classEntry);
+    	sessionStorage.setItem('getClassEntries', JSON.stringify(classEntry));
+    	this.router.navigate(['/grade-report']);
+    }
 }
--- CoolScan/src/app/home/home.page.ts
+++ CoolScan/src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController, LoadingController } from '@ionic/angular';
 import { MenuController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -10,25 +14,62 @@ import { ActivatedRoute } from '@angular/router';
 export class HomePage {
 
   constructor(private menu: MenuController,
-              private route: ActivatedRoute) {
-                this.route.queryParams.subscribe((res)=>{
-                  this.ids.firstname = res.firstname;
-                  this.ids.lastname = res.lastname;
-                });
+              private route: ActivatedRoute,
+              public alertCtrl: AlertController, 
+              public navCtrl: NavController,
+              private router: Router, 
+              public load: LoadingController,
+              private http: HttpClient) {
               }
-  username = '';
-  ids: any;
+
+  studentName = '';
+  studentID = '';
+  classIDs = [];
+  getClassUrl = 'http://localhost/csci150/getClasses.php';
+
   ngOnInit(){
-    this.username = sessionStorage.getItem('loggedUser');
+    this.studentName = sessionStorage.getItem('loggedUser');
+    this.studentID = sessionStorage.getItem('UserID');
   }
-}
 
-/*
+  async presentGetClassesError(error) {
+    const alert = await this.alertCtrl.create({
+      header: 'Failed To Get Grades',
+      message: 'Cannot Get Error: ' + error,
+      buttons: ['OK'],
+    });
+    
+    await alert.present();
+    let result=await alert.onDidDismiss();
+    console.log(result);
+    }
 
-*/
+  getClasses()
+  {
+    let res;
+      //Convert to Json string to send to php file
+      let data = JSON.stringify({
+          studentID: this.studentID,
+      });
+      //Post to php file and return success or fail
+      this.http.post(this.getClassUrl, data).subscribe(res=>{
+        //console.log(res);
+        if(res[0] == 'Get Classes Succes!')
+        {
+          console.log(res[0]);
+          this.classIDs = res[1];
+          sessionStorage.setItem('classes',JSON.stringify(this.classIDs));
+          console.log(this.classIDs);
+          this.router.navigate(['/grades']);
+        }
+        else
+        {
+          console.log(this.presentGetClassesError(res[0]));
+      }
+      }, error => {
+          console.log(this.presentGetClassesError(error));
+        });
+  }
 
-  constructor(private menu: MenuController) {}
-
-  logOut() {}
 }
 
