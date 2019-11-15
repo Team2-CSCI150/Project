eae4c7187cb24ef3c98df377c2e51aa7fd5a6489
Jesc32 <jesc32@mail.fresnostate.edu>
2019-11-14 19:51:10 -0800

    updated check-in

--- CoolScan/src/app/check-in/check-in.page.ts
+++ CoolScan/src/app/check-in/check-in.page.ts
@@ -7,6 +7,13 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 })
 
 export class CheckInPage implements OnInit {
+  //Definition of classroom coordinates
+  //36.814, -119.75
+  //let tgtLatitude = 36.814;
+  //let tgtLongitude = -119.75;
+  tgtLatitude = 36.791;
+  tgtLongitude =  -119.67;
+
   constructor(private geolocation: Geolocation) {}
 
   ngOnInit() {
@@ -15,32 +22,20 @@ export class CheckInPage implements OnInit {
   checkIn() {
     //GPS initialization
     this.geolocation.getCurrentPosition().then((resp) => {
-      
-      //Definition of classroom coordinates
-      //36.814, -119.75
-      //let tgtLatitude = 36.814;
-      //let tgtLongitude = -119.75;
-      var tgtLatitude = 36.815;
-      var tgtLongitude = -119.75;
-
-      
-
       //Retrieval of student's coordinates
       document.getElementById('demo1').innerHTML = "Latitude: " + resp.coords.latitude.toString();
       document.getElementById('demo2').innerHTML = "Longitude: " + resp.coords.longitude.toString();
       console.log(resp.coords.latitude, resp.coords.longitude);
-
       //Check if coordinates are the same
       //toPrecision() rounds the number to specified length (5 in this case)
-      //rounding is needed because different devices provide varying degrees of precision 
-      if(resp.coords.latitude.toPrecision(5) === tgtLatitude.toString()) {
-        if(resp.coords.longitude.toPrecision(5) === tgtLongitude.toString()) {
-          document.getElementById('demo3').innerHTML = "Check-in Success!";
-	}
+      //rounding is needed because different devices provide varying degrees of precision
+      if((resp.coords.latitude.toPrecision(5) === this.tgtLatitude.toString()) && (resp.coords.longitude.toPrecision(5) === this.tgtLongitude.toString())) {
+        document.getElementById('demo3').innerHTML = "Check-in Success!";
+	    } else {
+        document.getElementById('demo3').innerHTML = "Check-in Failed!";
       }
     }).catch((error) => {
       console.log('Error getting location', error);
     });
   }
-
 }
