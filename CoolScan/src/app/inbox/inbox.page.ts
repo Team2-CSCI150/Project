598696a1e23bd5feca399dfd89c35f1593b6c66e
DanielJacuinde <djacuinde96@mail.fresnostate.edu>
2019-12-03 16:17:50 -0800

    *Sockets

--- CoolScan/src/app/inbox/inbox.page.ts
+++ CoolScan/src/app/inbox/inbox.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController, LoadingController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import {environment, CLASSES_URL} from '../../environments/environment';
 
 @Component({
   selector: 'app-inbox',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inbox.page.scss'],
 })
 export class InboxPage implements OnInit {
+  classes = [];
+  teacher = '';
+  ID = '';
+  
+  constructor(
+	public alertCtrl: AlertController,
+	public navCtrl: NavController,
+    private router: Router,
+    public load: LoadingController,
+    private http: HttpClient ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+	ngOnInit() {
+		this.ID= sessionStorage.getItem('UserID');
+		//console.log (this.ID);
+		this.getClasses(this.ID);
+	}
+	
+	getClasses(ID){
+		let res;
+		let data = JSON.stringify({'studentID': ID});
+	
+		this.http.post(CLASSES_URL, data).subscribe(res=> {
+			if(res[0] == 'Get Classes Succes!'){
+				//console.log("This is res[1]: ");
+				console.log(res[1]);
+				
+				var Class = JSON.parse(JSON.stringify(res[1]));
+				console.log(Class);
+				var courses = [Class["510100"], Class["510102"], 
+						Class["510103"], Class["510104"]];
+				this.classes.push(courses);
+				console.log(courses);
+			}
+			else{
+				console.log("Failed getting classes! ");
+			}
+		});
+	}
+ 
+  setTeacher(teacher){
+	  sessionStorage.setItem('Teacher', this.teacher);
+	  this.router.navigateByUrl('/convo');
   }
-
 }
